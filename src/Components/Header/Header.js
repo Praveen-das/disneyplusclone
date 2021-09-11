1d552dc191cb2a361609dc836648c28ae1f80800
Praveen Das <praveendask97@gmail.com>
2021-09-11 17:13:31 +0530

    navbar drop down menu added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './header.css'
 import { Kids, Logo } from '../../assets/URLs/imgURLs'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
@@ -12,12 +13,36 @@ function Header() {
                         <span className='line'></span>
                         <span className='line'></span>
                     </div>
-                    <img className='disneyplus' src={Logo} alt="logo" />
-                    <a href="/#">TV</a>
-                    <a href="/#">Movies</a>
-                    <a href="/#">Sports</a>
-                    <a href="/#">Premium</a>
-                    <a href="/#">Disney+</a>
+                    <Link to='/'><img className='disneyplus' src={Logo} alt="logo" /></Link>
+                    <div className='dropDown'>
+                        <label htmlFor="">TV</label>
+                        <div className='dropDownContents'>
+                            <Link><h1>Hotstar Special</h1></Link>
+                            <Link><h1>Quix</h1></Link>
+                            <Link><h1>Star Jalsha</h1></Link>
+                            <Link><h1>StarPlus</h1></Link>
+                            <Link><h1>Star Vijay</h1></Link>
+                            <Link><h1>Star Bharat</h1></Link>
+                            <Link><h1>Asianet</h1></Link>
+                            <Link><h1>Star Maa</h1></Link>
+                            <Link><h1>more...</h1></Link>
+                        </div>
+                    </div>
+                    <div className='dropDown'>
+                        <label htmlFor="">Movies</label>
+                        <div className='dropDownContents'>
+                            <Link><h1>Hindi</h1></Link>
+                            <Link><h1>Bengali</h1></Link>
+                            <Link><h1>Telugu</h1></Link>
+                            <Link><h1>Malayalam</h1></Link>
+                            <Link><h1>Tamil</h1></Link>
+                            <Link><h1>Marathi</h1></Link>
+                            <Link><h1>English</h1></Link>
+                            <Link><h1>Kannada</h1></Link>
+                        </div>
+                    </div>
+                    <label href="/#">Premium</label>
+                    <label href="/#">Disney+</label>
                     <img className='kidsLogo' src={Kids} alt="kids" />
                 </div>
                 <div className="right">
